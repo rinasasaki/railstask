1bdd0e91ee6eef0c8e20ef7155b00483b8c7a605
佐々木　莉奈 <1072sasaki@gmail.com>
2021-08-29 13:56:09 +0900

    [add] todotask controller add index

--- task/app/controllers/todotask_controller.rb
+++ task/app/controllers/todotask_controller.rb
@@ -2,46 +2,4 @@ class TodotaskController < ApplicationController
   def index
     @todotask = Todotask.all
   end
-
-  def new
-    if request.post? then
-      Todotask.create(todotask_params)
-      goback
-    else
-      @todotask = Todotask.new
-    end
-  end
-
-  def edit
-    @todotask = Todotask.find(params[:id])
-    if request.patch? then
-      @todotask.update(todotask_params)
-      goback
-    end
-  end
-
-  def update
-    @todotask = Todotask.find(params[:id]) 　#①変数に更新したいレコード情報を代入
-    @todotask.update(todotask_params) 　#②updateメソッドを実行する
-  end
-
-  def delete
-    Todotask.find(params[:id]).destroy
-    goback
-  end
-
-  def show
-    @todotask = Todotask.find(params[:id])
-  end
-end
-
-private
-def todotask_params
-  params.require(:todotask).permit(:title, :content)
-end
-
-def goback
-  redirect_to '/todotask'
-end
-
 end
